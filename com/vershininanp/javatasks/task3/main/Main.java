07460b072347bff043d8318bdfa48e3bf098f977
Nataliya <bulavina@mera.ru>
2016-09-19 16:28:35 +0300

    added remarks task 4 part 2

--- com/vershininanp/javatasks/task3/main/Main.java
+++ com/vershininanp/javatasks/task3/main/Main.java
@@ -11,21 +11,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		try { 			
-			DepartmentRoom[] departmentRooms = {new EducationalRoom(1, "10"), new EducationalRoom(1, "13"), 
-												new DeansOffice(1, "12", "Ivanov"), new DeansOffice(1, "16", "Petrov"), 
-												new LectureRoom(1, "10", 15), new LectureRoom(1, "14", 10)};
+			DepartmentRoom[] departmentRooms = {new EducationalRoom(1, "10"), new EducationalRoom(1, "11"), 
+												new DeansOffice(1, "13", "Ivanov"), new DeansOffice(1, "16", "Petrov"), 
+												new LectureRoom(1, "13", 15), new LectureRoom(1, "14", 10)};
 			
 			ArrayList<LectureRoom> lectureRooms = new ArrayList();
 		
 			for(int i = 0; i < departmentRooms.length; i++)	{
-				departmentRooms[i].open(); 
-				
-				//compare number of room and exception
 				for(int j = 0; (j < departmentRooms.length) && (j != i); j++) {
-					if((departmentRooms[i].equals(departmentRooms[j])) && (departmentRooms[i].getClass() != departmentRooms[j].getClass())) {
+					if((departmentRooms[i].equals(departmentRooms[j])) 
+							&& (departmentRooms[i].getClass() != departmentRooms[j].getClass())
+							&& (departmentRooms[j].getClass().getName() != "com.vershininanp.javatasks.task3.educationalroom.EducationalRoom")) {
 						throw new DublicateException("Exception! There was a duplicate of the room!");
 					} 
 				}
+				
+				departmentRooms[i].open(); 
 								
 				if (departmentRooms[i] instanceof LectureRoom) {
 					lectureRooms.add((LectureRoom)departmentRooms[i]);
